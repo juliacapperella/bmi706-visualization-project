94a08f54500f28e3d2d20fa26de6829fedca1141
Julia Capperella <juliacapperella@gmail.com>
2024-10-16 14:28:03 -0400

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.set_page_config(layout="wide")
 @st.cache_data
 def load_data():
     bci_df = pd.read_csv("https://raw.githubusercontent.com/juliacapperella/bmi706-visualization-project/refs/heads/main/final_data.csv")
-
+    bci_df = bci_df.rename(columns={'CountryTerritoryArea': 'Country'})
     return bci_df
 
 df = load_data()
@@ -32,9 +32,9 @@ default = [
 ]
 
 ## Make country selector
-countries = st.sidebar.multiselect("Countries",df["CountryTerritoryArea"].unique(), default)
-subset = subset[subset["CountryTerritoryArea"].isin(countries)]
-subset2 = df[df["CountryTerritoryArea"].isin(countries)]
+countries = st.sidebar.multiselect("Countries",df["Country"].unique(), default)
+subset = subset[subset["Country"].isin(countries)]
+subset2 = df[df["Country"].isin(countries)]
 
 ## Make infectious syndrome selector
 infection = st.sidebar.selectbox("Infectious Syndrome",df["Infectious Syndrome"].unique())
@@ -47,7 +47,7 @@ subset = subset[subset["Bacterial Pathogen"] == bacteria]
 ## Create chart 1 -- heat map of BCI for selection options
 chart = alt.Chart(subset).mark_rect().encode(
     x=alt.X("Antibiotic Group"),
-    y=alt.Y("CountryTerritoryArea"),
+    y=alt.Y("Country"),
     color=alt.Color("BCIs per million population:Q", scale=alt.Scale(type="log",domain=(0.01, 1000), clamp=True),
                     title="BCIs per million population"),
     tooltip=["BCIs per million population"],
@@ -57,9 +57,9 @@ chart = alt.Chart(subset).mark_rect().encode(
 
 ## Create chart 2 -- bar chart of health expenditure for selection options
 chart2 = alt.Chart(subset).mark_bar().encode(
-    x=alt.X("gghed_gdp", title="Domestic Health Expenditure as Percent of GDP", axis=alt.Axis(tickMinStep=50000000)),
-    y=alt.Y("CountryTerritoryArea", sort="-x"),
-    tooltip=["gghed_gdp","CountryTerritoryArea"],
+    x=alt.X("mean(gghed_gdp)", title="Domestic Health Expenditure as Percent of GDP", axis=alt.Axis(tickMinStep=50000000)),
+    y=alt.Y("Country", sort="-x"),
+    tooltip=[alt.Tooltip("mean(gghed_gdp)", title="Health Expenditure"),"Country"],
 )
 
 ## Create chart 3 -- (unlinked) line chart of year and health expenditure
@@ -67,11 +67,11 @@ chart3 = alt.Chart(subset2).mark_line(
     point=True
 ).encode(
     x=alt.X("Year:N", axis=alt.Axis(title='Year')),
-    y=alt.Y("gghed_gdp:Q"),
-    color=alt.Color('CountryTerritoryArea:N'),
-    tooltip=["gghed_gdp"]
+    y=alt.Y("gghed_gdp:Q", axis=alt.Axis(title='Health Expenditure')),
+    color=alt.Color('Country:N'),
+    tooltip=[alt.Tooltip("gghed_gdp", title="Health Expenditure")]
 ).properties(
-    title=f"Health expenditures per year",
+    title=f"Domestic Health Expenditure as Percent of GDP per year",
     width=300
 )
 
@@ -80,9 +80,9 @@ chart4 = alt.Chart(subset2).mark_line(
     point=True
 ).encode(
     x=alt.X("Year:N", axis=alt.Axis(title='Year')),
-    y=alt.Y("sum(BCIs per million population):Q"),
-    color=alt.Color('CountryTerritoryArea:N'),
-    tooltip=["sum(BCIs per million population)"]
+    y=alt.Y("sum(BCIs per million population):Q", axis=alt.Axis(title="BCIs per million population")),
+    color=alt.Color('Country:N'),
+    tooltip=[alt.Tooltip("sum(BCIs per million population)", title = "BCIs per million pop")]
 ).properties(
     title=f"BCIs per million population per year",
     width=300
@@ -98,13 +98,13 @@ st.altair_chart(charttotal, use_container_width=True)
 linecharts = chart3 | chart4
 st.altair_chart(linecharts, use_container_width=True)
 
-# countries_in_subset = subset["CountryTerritoryArea"].unique()
-# if len(countries_in_subset) != len(countries):
-#     if len(countries_in_subset) == 0:
-#         st.write("No data avaiable for given subset.")
-#     else:
-#         missing = set(countries) - set(countries_in_subset)
-#         st.write("No data available for " + ", ".join(missing) + ".")
+countries_in_subset = subset["Country"].unique()
+if len(countries_in_subset) != len(countries):
+    if len(countries_in_subset) == 0:
+        st.write("No data avaiable for given subset.")
+    else:
+        missing = set(countries) - set(countries_in_subset)
+        st.write("No data available for " + ", ".join(missing) + ".")
 
 
 
